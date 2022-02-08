78e92c1b8b273ff9f90e4bd4d39fc4955885d98b
Dyburin <sashaudod5@gmail.com>
2022-02-08 19:27:09 +0300

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-g7s*4@)5yq95tclta)!t@(-7!gqwih@ez#l7(l!-^lc7(-t0vt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pytonanywhere.com']
 
